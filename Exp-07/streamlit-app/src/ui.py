874088572092a74658ee6c8ee6b6fd46068ba82c
Uttkarsh Sharma <168623259+Uttkarshsh@users.noreply.github.com>
2025-04-30 14:32:07 +0530

    Update ui.py

--- Exp-07/streamlit-app/src/ui.py
+++ Exp-07/streamlit-app/src/ui.py
@@ -48,20 +48,21 @@ def display_sidebar_header() -> None:
         logo = None
 
     with st.sidebar:
-        st.image(logo, use_column_width=True)
+        st.image(logo, use_container_width=True)
         col1, col2 = st.columns(2)
         repo_link: Text = (
             "https://github.com/mnrozhkov/evidently/tree/main/examples/integrations"
         )
         evidently_docs: Text = "https://docs.evidentlyai.com/"
         col1.markdown(
-            f"<a style='display: block; text-align: center;' href={repo_link}>Source code</a>",
+            f"<a style='display: block; text-align: center;' href=\"{repo_link}\">Source code</a>",
             unsafe_allow_html=True,
         )
         col2.markdown(
-            f"<a style='display: block; text-align: center;' href={evidently_docs}>Evidently docs</a>",
+            f"<a style='display: block; text-align: center;' href=\"{evidently_docs}\">Evidently docs</a>",
             unsafe_allow_html=True,
         )
+
         st.header("")  # add space between logo and selectors
 
 
